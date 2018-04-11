99fdd85fac79d17be1c4d099378ef6b9fb25c310
森田賢二 <kenjimoritata@gmail.com>
2018-04-12 07:50:38 +0900

    fix flow jest

---
+++ src/container/Topic.test.js
@@ -0,0 +1,7 @@
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import Topic from './Topic';
+
+describe('Topick', () => {
+  it('render', () => {});
+});
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,8 +1,10 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { topic } from './topic';
 
-const reducers =  combineReducers({
-    routing: routerReducer,
+const reducers = combineReducers({
+  routing: routerReducer,
+  topic: topic
 });
 
 export default reducers;
